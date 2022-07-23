d965275cd4004e0b9d1b918054ddc309a30494ea
Shchepetilnikov <kalugalina.gugl@gmail.com>
2022-07-23 17:26:03 +0300

    added recursion)

---
+++ DigitInNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿double DigitsInNumber(int number)
+{
+    
+    if(number / 10 > 0)
+    {
+        return 1 + DigitsInNumber(number / 10);
+        
+    }
+    else return 1;
+}
+
+Console.WriteLine("Num");
+int number = Convert.ToInt32(Console.ReadLine());
+
+double result = DigitsInNumber(number);
+
+
+Console.WriteLine(DigitsInNumber(number));
---
+++ NatureDistance/Program.cs
@@ -0,0 +1,16 @@
+﻿int NatureDistance(int m, int n)
+{
+    if(n >= m)
+    {
+        return n + NatureDistance(m, n - 1);
+    }
+    else return 0;
+}
+
+Console.WriteLine("Num");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Num");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NatureDistance(m, n));
